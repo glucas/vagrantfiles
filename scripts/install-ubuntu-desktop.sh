507c1a27e0e0537b56f3de42a6f6a52f8d74cce0
Greg Lucas <greg@glucas.net>
2014-03-06 14:06:49 -0500

    Generate script to toggle scale factor.

--- scripts/install-ubuntu-desktop.sh
+++ scripts/install-ubuntu-desktop.sh
@@ -3,9 +3,31 @@
 # install minimal desktop on top of ubuntu server
 apt-get install -y --no-install-recommends ubuntu-desktop
 
-# default terminal font doesn't render well for me, so change it:
-su -c "gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 13'" vagrant
-
-
 # add vagrant user to admin group
 usermod -a -G admin vagrant
+
+# default terminal font doesn't render properly, so change it:
+echo "gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 13'" >> /home/vagrant/.bashrc
+
+# script to toggle text scale factor
+TOGGLE_SCALE=/home/vagrant/bin/toggle-scale-factor.sh
+if [ ! -f $TOGGLE_SCALE ]; then
+    cat <<"EOF" > $TOGGLE_SCALE
+#!/bin/bash
+# Toggle text scale factor betwen 0.9 (laptop) and 0.7 (monitor)
+
+CURRENT=`gsettings get org.gnome.desktop.interface text-scaling-factor`
+CURRENT=${CURRENT:0:3}
+
+NEW=0.9
+if [ "$NEW" == "$CURRENT" ]; then
+    NEW=0.7
+fi
+
+gsettings set org.gnome.desktop.interface text-scaling-factor $NEW
+
+echo "Set text scaling factor to $NEW."
+EOF
+    chown vagrant:vagrant $TOGGLE_SCALE
+    echo "Generated file: $TOGGLE_SCALE"
+fi
