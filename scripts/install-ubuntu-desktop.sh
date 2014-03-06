3bb7315f2e41c1418e1db80d4314e48cfb1efd77
Greg Lucas <greg@glucas.net>
2014-03-06 11:40:16 -0500

    Move admin group stuff to install-desktop.

--- scripts/install-ubuntu-desktop.sh
+++ scripts/install-ubuntu-desktop.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # install minimal desktop on top of ubuntu server
-
 apt-get install -y --no-install-recommends ubuntu-desktop
 
 # default terminal font doesn't render well for me, so change it:
-
 su -c "gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 13'" vagrant
+
+
+# add vagrant user to admin group
+usermod -a -G admin vagrant
